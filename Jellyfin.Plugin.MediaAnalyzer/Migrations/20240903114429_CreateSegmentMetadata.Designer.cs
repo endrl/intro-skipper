b36f826eed31d918076ae39528b8fbd77706093f
endrl <119058008+endrl@users.noreply.github.com>
2024-09-03 13:47:21 +0200

    feat: add db migration

--- Jellyfin.Plugin.MediaAnalyzer/Migrations/20240903114429_CreateSegmentMetadata.Designer.cs
+++ Jellyfin.Plugin.MediaAnalyzer/Migrations/20240903114429_CreateSegmentMetadata.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Jellyfin.Plugin.MediaAnalyzer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jellyfin.Plugin.MediaAnalyzer.Migrations
 {
     [DbContext(typeof(MediaAnalyzerDbContext))]
-    partial class MediaAnalyzerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240903114429_CreateSegmentMetadata")]
+    partial class CreateSegmentMetadata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
---
+++ Jellyfin.Plugin.MediaAnalyzer/Migrations/20240903114429_CreateSegmentMetadata.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfin.Plugin.MediaAnalyzer.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateSegmentMetadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlacklistSegment");
+
+            migrationBuilder.CreateTable(
+                name: "SegmentMetadata",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    SeriesName = table.Column<string>(type: "TEXT", nullable: false),
+                    SegmentId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ItemId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    PreventAnalyzing = table.Column<bool>(type: "INTEGER", nullable: false),
+                    AnalyzerType = table.Column<int>(type: "INTEGER", nullable: true),
+                    AnalyzerNote = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SegmentMetadata", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SegmentMetadata_ItemId",
+                table: "SegmentMetadata",
+                column: "ItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SegmentMetadata");
+
+            migrationBuilder.CreateTable(
+                name: "BlacklistSegment",
+                columns: table => new
+                {
+                    ItemId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlacklistSegment", x => new { x.ItemId, x.Type });
+                });
+        }
+    }
+}
